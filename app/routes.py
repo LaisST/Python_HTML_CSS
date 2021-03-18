15822d63328a2f61792167e4182803239d967e80
Lais.ST <Laiscostast@gmail.com>
2021-03-18 19:28:55 +0000

    index_user

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
 from app import app
+from flask import render_template
+
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello World!"
+    user = {'username': 'Lais'}
+    return render_template("index.html", user=user)
